3b51c77f60b6520706b5ef7c610ceaed238bee86
yxluo <yuxiangluo2003@gmail.com>
2024-11-04 15:29:03 +0800

    feat/add snippet

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "@/App";
 import "normalize.css";
-import "./assets/css/index.less"
+import "./assets/css/index.less";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
-root.render((
+root.render(
   <HashRouter>
-    <App/>
-  </HashRouter>
-));
+    <App />
+  </HashRouter>,
+);
