36102a86c7b39bf47ddcd959e70bcb93962b2b25
yxluo <yuxiangluo2003@gmail.com>
2024-11-04 16:02:06 +0800

    feat/lazy import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {useRoutes} from "react-router-dom";
-import routes from "@/router"
+import { useRoutes } from "react-router-dom";
+import routes from "@/router";
 
 function App() {
   return <div className="App">{useRoutes(routes)}</div>;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,22 @@
-import {RouteObject} from "react-router-dom";
-import Home from "@/views/home";
-import Login from "@/views/login";
+import React, { lazy } from "react";
+import { Navigate, RouteObject } from "react-router-dom";
+
+const Home = lazy(() => import("@/views/home"));
+const Login = lazy(() => import("@/views/login"));
 
 const routes: RouteObject[] = [
   {
+    path: "/",
+    element: <Navigate to="/home" />,
+  },
+  {
     path: "/home",
-    element: <Home/>
+    element: <Home />,
   },
   {
     path: "/login",
-    element: <Login/>
-  }
-]
+    element: <Login />,
+  },
+];
 
-
-export default routes
+export default routes;
